e19724fcb4607fa709d06677ec4df5aa65233685
DanielaLCM <danylu25@hotmail.com>
2020-02-13 15:32:21 -0600

    initial commit

---
+++ Assets/Scripts/QuitOnEscape.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+
+public class QuitOnEscape : MonoBehaviour
+{
+
+    // Update is called once per frame
+    private void Start()
+    {
+        Debug.Log("Start se llamo");
+    }
+    void Update()
+    {
+
+       // Debug.Log("Update se llamo");
+        if (Input.GetKey ("escape"))
+            {
+            Debug.Log("escape se llamo");
+            Application.Quit();
+            }
+    }
+}
---
+++ Temp/il2cppOutput/il2cppOutput/Assembly-CSharp_CodeGen.c
@@ -0,0 +1,95 @@
+﻿#include "il2cpp-config.h"
+
+#ifndef _MSC_VER
+# include <alloca.h>
+#else
+# include <malloc.h>
+#endif
+
+
+
+#include "codegen/il2cpp-codegen-metadata.h"
+
+
+
+
+
+IL2CPP_EXTERN_C_BEGIN
+IL2CPP_EXTERN_C_END
+
+
+
+
+// 0x00000001 System.Void CameraController::Start()
+extern void CameraController_Start_mB6CB2C21A2848E4C149B512DBC2126404E75EB4B ();
+// 0x00000002 System.Void CameraController::LateUpdate()
+extern void CameraController_LateUpdate_m633E06D1E1CC1B4CBFCCEB8A89A46E51B54504AA ();
+// 0x00000003 System.Void CameraController::.ctor()
+extern void CameraController__ctor_m3E87BE2198B66EDA7ECA07CF01545ABD1B4761DE ();
+// 0x00000004 System.Void PlayerController::Start()
+extern void PlayerController_Start_mC0C9B9461D0BDAC48EC43715818A4BA63C4F45EF ();
+// 0x00000005 System.Void PlayerController::FixedUpdate()
+extern void PlayerController_FixedUpdate_m914EA3E3CCE4DF6AEB2E78317FFC1D507DACEBDE ();
+// 0x00000006 System.Void PlayerController::OnTriggerEnter(UnityEngine.Collider)
+extern void PlayerController_OnTriggerEnter_m2F6A24BACBA236DC01D4689BD68C270C29607E44 ();
+// 0x00000007 System.Void PlayerController::SetCountText()
+extern void PlayerController_SetCountText_m1FD01D1E24C760C9FF66E805566B241D87662E11 ();
+// 0x00000008 System.Void PlayerController::.ctor()
+extern void PlayerController__ctor_m648E40092E37395F4307411E855445886113CD60 ();
+// 0x00000009 System.Void QuitOnEscape::Start()
+extern void QuitOnEscape_Start_m5FB1CF7E254A99E5BB695566A1B11D75E8039D86 ();
+// 0x0000000A System.Void QuitOnEscape::Update()
+extern void QuitOnEscape_Update_mCDDF2AFEEAE08DB2ED716A1512F7028DFCBA3E3E ();
+// 0x0000000B System.Void QuitOnEscape::.ctor()
+extern void QuitOnEscape__ctor_m012EE670D3DB49E94B443EFD4503D58A08A4C77F ();
+// 0x0000000C System.Void Rotator::Update()
+extern void Rotator_Update_mD04906B2AEDE9F526E59D28F39F6DB078EE8BDD6 ();
+// 0x0000000D System.Void Rotator::.ctor()
+extern void Rotator__ctor_m1529510FC18D463FF7B3D27951F78ED3BDE25E90 ();
+static Il2CppMethodPointer s_methodPointers[13] = 
+{
+	CameraController_Start_mB6CB2C21A2848E4C149B512DBC2126404E75EB4B,
+	CameraController_LateUpdate_m633E06D1E1CC1B4CBFCCEB8A89A46E51B54504AA,
+	CameraController__ctor_m3E87BE2198B66EDA7ECA07CF01545ABD1B4761DE,
+	PlayerController_Start_mC0C9B9461D0BDAC48EC43715818A4BA63C4F45EF,
+	PlayerController_FixedUpdate_m914EA3E3CCE4DF6AEB2E78317FFC1D507DACEBDE,
+	PlayerController_OnTriggerEnter_m2F6A24BACBA236DC01D4689BD68C270C29607E44,
+	PlayerController_SetCountText_m1FD01D1E24C760C9FF66E805566B241D87662E11,
+	PlayerController__ctor_m648E40092E37395F4307411E855445886113CD60,
+	QuitOnEscape_Start_m5FB1CF7E254A99E5BB695566A1B11D75E8039D86,
+	QuitOnEscape_Update_mCDDF2AFEEAE08DB2ED716A1512F7028DFCBA3E3E,
+	QuitOnEscape__ctor_m012EE670D3DB49E94B443EFD4503D58A08A4C77F,
+	Rotator_Update_mD04906B2AEDE9F526E59D28F39F6DB078EE8BDD6,
+	Rotator__ctor_m1529510FC18D463FF7B3D27951F78ED3BDE25E90,
+};
+static const int32_t s_InvokerIndices[13] = 
+{
+	23,
+	23,
+	23,
+	23,
+	23,
+	26,
+	23,
+	23,
+	23,
+	23,
+	23,
+	23,
+	23,
+};
+extern const Il2CppCodeGenModule g_AssemblyU2DCSharpCodeGenModule;
+const Il2CppCodeGenModule g_AssemblyU2DCSharpCodeGenModule = 
+{
+	"Assembly-CSharp.dll",
+	13,
+	s_methodPointers,
+	s_InvokerIndices,
+	0,
+	NULL,
+	0,
+	NULL,
+	0,
+	NULL,
+	NULL,
+};
